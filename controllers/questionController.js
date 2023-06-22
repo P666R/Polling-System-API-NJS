4297a5b9867f2b723bcaae394d5ae5256df2b6f9
PRoy <sam469roger@gmail.com>
2023-06-23 00:00:15 +0530

    delete question

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -29,10 +29,20 @@ exports.createQuestion = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteQuestion = catchAsync(async (req, res, next) => {
-  const question = await Question.findByIdAndDelete(req.params.id);
+  const question = await Question.findById(req.params.id).populate('options');
 
   if (!question) {
     return next(new AppError('No question found with this id!', 404));
+  }
+
+  const totalVotes = question.options.reduce((acc, option) => {
+    return acc + option.vote;
+  }, 0);
+
+  if (totalVotes > 0) {
+    return next(
+      new AppError('Cannot delete question whose options have votes!', 400)
+    );
   }
 
   res.status(204).json({
