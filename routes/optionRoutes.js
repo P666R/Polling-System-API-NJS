ef1964e91b67dd04c1a52a81337922b02eb96623
PRoy <sam469roger@gmail.com>
2023-06-23 09:00:24 +0530

    added comments to routes

--- routes/optionRoutes.js
+++ routes/optionRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const optionController = require('../controllers/optionController');
 
+// Create a new router instance
 const router = express.Router({ mergeParams: true });
 
+// Define routes for creating, deleting options and adding votes to options
 router.route('/create').post(optionController.createOption);
 router.route('/:id/add_vote').post(optionController.addVoteOption);
 router.route('/:id/delete').delete(optionController.deleteOption);
--- routes/questionRoutes.js
+++ routes/questionRoutes.js
@@ -2,10 +2,13 @@ const express = require('express');
 const questionController = require('../controllers/questionController');
 const optionRoutes = require('./optionRoutes');
 
+// Create a new router instance
 const router = express.Router();
 
+// Use the optionRoutes for any requests with a path that matches '/:id/options'
 router.use('/:id/options', optionRoutes);
 
+// Define routes for creating, getting, and deleting questions
 router.route('/create').post(questionController.createQuestion);
 router.route('/:id').get(questionController.getQuestion);
 router.route('/:id/delete').delete(questionController.deleteQuestion);
