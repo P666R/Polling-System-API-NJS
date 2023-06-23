be001dcca8bcdc5d6e85c96ca4769ecffc372971
PRoy <sam469roger@gmail.com>
2023-06-23 08:54:16 +0530

    added comments to AppError & catchAsync

--- utils/appError.js
+++ utils/appError.js
@@ -1,10 +1,24 @@
+/**
+ * Custom error class for application errors
+ * @class
+ * @extends Error
+ */
 class AppError extends Error {
+  /**
+   * Constructor for AppError
+   * @constructor
+   * @param {string} message - Error message
+   * @param {number} statusCode - HTTP status code for the error
+   */
   constructor(message, statusCode) {
     super(message);
     this.statusCode = statusCode;
+    // Determine if the error is a client error (4xx) or server error (5xx)
     this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    // Indicates if the error is operational (can be handled by the application) or a programming error
     this.isOperational = true;
 
+    // Capture stack trace for debugging purposes
     Error.captureStackTrace(this, AppError);
   }
 }
--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,3 +1,5 @@
 module.exports = (fn) => (req, res, next) => {
+  // Call the original async route handler function with the provided request, response, and next parameters.
+  // If the function throws an error, catch it and pass it to the global error handling middleware.
   fn(req, res, next).catch(next);
 };
